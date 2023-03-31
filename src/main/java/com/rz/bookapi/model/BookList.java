65284c7c7a0c508d59e0a79c6567ee76ccbb2a2d
rafzoio <raphael.zoio@hotmail.com>
2023-03-31 13:20:09 +0100

    Added lombok dependency for reduced boilerplate code.

--- src/main/java/com/rz/bookapi/model/BookList.java
+++ src/main/java/com/rz/bookapi/model/BookList.java
@@ -1,37 +1,24 @@
 package com.rz.bookapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "bookList")
-public class BookList {
 
+public class BookList {
     @XmlElement(name = "book")
     private List<Book> books;
-
-    public BookList(List<Book> books) {
-        this.books = books;
-    }
-
-    public BookList() {
-    }
-
-    public List<Book> getBooks() {
-        return books;
-    }
-
-    public void setBooks(List<Book> books) {
-        this.books = books;
-    }
-
-    @Override
-    public String toString() {
-        return "BookList{" +
-                "books=" + books +
-                '}';
-    }
 }
+
+
